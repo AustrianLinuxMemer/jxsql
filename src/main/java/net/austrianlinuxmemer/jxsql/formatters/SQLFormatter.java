26b15252d922410d8e50679cc245ae2b8a406f24
leo <leo.fuereder@outlook.com>
2025-02-25 18:16:41 +0100

    SQLFormatter method names renamed

--- src/main/java/net/austrianlinuxmemer/jxsql/formatters/SQLFormatter.java
+++ src/main/java/net/austrianlinuxmemer/jxsql/formatters/SQLFormatter.java
@@ -10,73 +10,73 @@ public interface SQLFormatter {
     String format(Object o, SQLFormatter childFormatter);
     SQLFormatter defaultFormatter = (o, f) -> switch (o) {
         case null -> "NULL";
-        case CharSequence c -> SQLFormatter.parseString(c);
+        case CharSequence c -> SQLFormatter.formatString(c);
         case Number n -> n.toString();
         case Boolean b -> b ? "TRUE" : "FALSE";
-        case Collection<?> c -> SQLFormatter.parseCollection(c, f);
-        case Object[] a -> SQLFormatter.parseArray(a, f);
-        case long[] a -> SQLFormatter.parseArray(a, f);
-        case int[] a -> SQLFormatter.parseArray(a, f);
-        case short[] a -> SQLFormatter.parseArray(a, f);
-        case byte[] a -> SQLFormatter.parseArray(a, f);
-        case char[] a -> SQLFormatter.parseArray(a, f);
-        case boolean[] a -> SQLFormatter.parseArray(a, f);
-        case float[] a -> SQLFormatter.parseArray(a, f);
-        case double[] a -> SQLFormatter.parseArray(a, f);
+        case Collection<?> c -> SQLFormatter.formatCollection(c, f);
+        case Object[] a -> SQLFormatter.formatArray(a, f);
+        case long[] a -> SQLFormatter.formatArray(a, f);
+        case int[] a -> SQLFormatter.formatArray(a, f);
+        case short[] a -> SQLFormatter.formatArray(a, f);
+        case byte[] a -> SQLFormatter.formatArray(a, f);
+        case char[] a -> SQLFormatter.formatArray(a, f);
+        case boolean[] a -> SQLFormatter.formatArray(a, f);
+        case float[] a -> SQLFormatter.formatArray(a, f);
+        case double[] a -> SQLFormatter.formatArray(a, f);
         default -> throw SQLFormatter.noFormattingCase.apply(o.getClass());
     };
-    static String parseString(CharSequence s) {
+    static String formatString(CharSequence s) {
         return "'" + s.codePoints().mapToObj(Character::toString).map(c -> c.equals("'") ? "''" : c).collect(Collectors.joining()) + "'";
     }
-    static String parseCollection(Collection<?> collection, SQLFormatter formatter) {
+    static String formatCollection(Collection<?> collection, SQLFormatter formatter) {
         return collection.stream().map(o -> formatter.format(o, formatter)).collect(Collectors.joining(","));
     }
-    static String parseArray(Object[] array, SQLFormatter formatter) {
+    static String formatArray(Object[] array, SQLFormatter formatter) {
         return Arrays.stream(array).map(o -> formatter.format(o, formatter)).collect(Collectors.joining(","));
     }
-    static String parseArray(long[] array, SQLFormatter formatter) {
+    static String formatArray(long[] array, SQLFormatter formatter) {
         return Arrays.stream(array).mapToObj(l -> formatter.format(l, formatter)).collect(Collectors.joining(","));
     }
-    static String parseArray(int[] array, SQLFormatter formatter) {
+    static String formatArray(int[] array, SQLFormatter formatter) {
         return Arrays.stream(array).mapToObj(i -> formatter.format(i, formatter)).collect(Collectors.joining(","));
     }
-    static String parseArray(short[] array, SQLFormatter formatter) {
+    static String formatArray(short[] array, SQLFormatter formatter) {
         Short[] boxed = new Short[array.length];
         for (int i = 0; i < boxed.length; i++) {
             boxed[i] = array[i];
         }
-        return parseArray(boxed, formatter);
+        return formatArray(boxed, formatter);
     }
-    static String parseArray(byte[] array, SQLFormatter formatter) {
+    static String formatArray(byte[] array, SQLFormatter formatter) {
         Byte[] boxed = new Byte[array.length];
         for (int i = 0; i < boxed.length; i++) {
             boxed[i] = array[i];
         }
-        return parseArray(boxed, formatter);
+        return formatArray(boxed, formatter);
     }
-    static String parseArray(boolean[] array, SQLFormatter formatter) {
+    static String formatArray(boolean[] array, SQLFormatter formatter) {
         Boolean[] boxed = new Boolean[array.length];
         for (int i = 0; i < boxed.length; i++) {
             boxed[i] = array[i];
         }
-        return parseArray(boxed, formatter);
+        return formatArray(boxed, formatter);
     }
-    static String parseArray(float[] array, SQLFormatter formatter) {
+    static String formatArray(float[] array, SQLFormatter formatter) {
         Float[] boxed = new Float[array.length];
         for (int i = 0; i < boxed.length; i++) {
             boxed[i] = array[i];
         }
-        return parseArray(boxed, formatter);
+        return formatArray(boxed, formatter);
     }
-    static String parseArray(double[] array, SQLFormatter formatter) {
+    static String formatArray(double[] array, SQLFormatter formatter) {
         return Arrays.stream(array).mapToObj(d -> formatter.format(d, formatter)).collect(Collectors.joining(","));
     }
-    static String parseArray(char[] array, SQLFormatter formatter) {
+    static String formatArray(char[] array, SQLFormatter formatter) {
         Character[] boxed = new Character[array.length];
         for (int i = 0; i < boxed.length; i++) {
             boxed[i] = array[i];
         }
-        return parseArray(boxed, formatter);
+        return formatArray(boxed, formatter);
     }
     Function<Class<?>, IllegalArgumentException> noFormattingCase = (c) -> new IllegalArgumentException(String.format("for Class %s does not exist a formatting case", c.getName()));
 }
